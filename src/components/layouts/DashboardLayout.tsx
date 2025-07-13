dd32dda7d4aa1f3df91734fa9bf1d41c1a157a83
BobBobilion <30764765+BobBobilion@users.noreply.github.com>
2025-07-12 19:13:20 -0500

    updated paddingfor thread page

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -109,7 +109,7 @@ export const DashboardLayout: React.FC = () => {
         )}
 
         {/* Page Content */}
-        <main className="px-4 sm:px-6 lg:px-8 py-8">
+        <main className={location.pathname.includes('/thread/') ? '' : 'px-4 sm:px-6 lg:px-8 py-8'}>
           <Outlet />
         </main>
       </div>
