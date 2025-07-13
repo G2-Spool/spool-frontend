d4f740c20b75e1e789751e42bbd07c0b9054b0e6
BobBobilion <30764765+BobBobilion@users.noreply.github.com>
2025-07-13 16:00:34 -0500

    Reapply "Merge branch 'main' of https://github.com/G2-Spool/spool-frontend"
    This reverts commit b89c359755388dcc3d9d7adbcbc61f15708365de.

--- src/pages/StudentDashboard.tsx
+++ src/pages/StudentDashboard.tsx
@@ -469,7 +469,11 @@ export const StudentDashboard: React.FC = () => {
             </p>
             <Button
               variant="primary"
-              onClick={() => setShowInterestModal(true)}
+              onClick={() => {
+                console.log('Opening interest modal with user:', user);
+                console.log('User ID:', user?.id);
+                setShowInterestModal(true);
+              }}
               className="text-white !text-white"
             >
               <MessageCircle className="h-4 w-4 mr-2 text-white" />
